e815a8025af08e97f70cd21774539fe867b7f78b
권빈 <95688224+GGaBBi11@users.noreply.github.com>
2022-03-03 21:16:25 +0900

    0303

---
+++ Platformer2D/Assets/02.Scripts/FollowCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform target;
+    private Transform tr;
+
+    private void Awake()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
