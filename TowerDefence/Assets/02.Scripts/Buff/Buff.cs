e0bfabc81d2e79b6739febffffe1be69524c0dd7
권빈 <95688224+GGaBBi11@users.noreply.github.com>
2022-03-29 21:56:20 +0900

    0329

---
+++ TowerDefence/Assets/02.Scripts/Buff/Buff.cs
@@ -0,0 +1,26 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buff : MonoBehaviour
+{
+    public float duration; // 지속시간
+    public bool doBuff = true;
+
+    public virtual void OnActive(Enemy target)
+    {
+        
+    }
+    
+    public virtual void OnDeactive(Enemy target)
+    {
+
+    }
+
+    public virtual bool OnDuration(Enemy target)
+    {
+
+        return doBuff;
+    }
+}
